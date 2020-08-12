fbf3e38e64314dc99d2c081bf1911ca1a1b25aa1
vagnersaller <vsaller@yahoo.com>
2020-08-12 14:17:55 -0300

    api: change test to deal with extra field

--- api/user/service_test.go
+++ api/user/service_test.go
@@ -16,30 +16,33 @@ func TestUpdateDataUser(t *testing.T) {
 
 	ctx := context.TODO()
 
+	oldPassword:="olpass123"
+	newPassword:="newpass123"
+
 	updateUser1 := &models.User{Name: "name", Email: "", Username: "newusername", Password: "", TenantID: "tenant"}
 	updateUser2 := &models.User{Name: "name", Email: "new@email.com", Username: "", Password: "", TenantID: "tenant"}
 	updateUser3 := &models.User{Name: "name", Email: "", Username: "", Password: "newpassword", TenantID: "tenant"}
 
 	//Change username
-	mock.On("UpdateUser", ctx, updateUser1.Username, updateUser1.Email, updateUser1.Password, updateUser1.TenantID).Return(nil).Once()
+	mock.On("UpdateUser", ctx, updateUser1.Username, updateUser1.Email, updateUser1.Password, updateUser1.Password, updateUser1.TenantID).Return(nil).Once()
 
-	err := s.UpdateDataUser(ctx, updateUser1.Username, updateUser1.Email, updateUser1.Password, updateUser1.TenantID)
+	err := s.UpdateDataUser(ctx, updateUser1.Username, updateUser1.Email, updateUser1.Password, updateUser1.Password, updateUser1.TenantID)
 
 	assert.NoError(t, err)
 	mock.AssertExpectations(t)
 
 	// changed email
-	mock.On("UpdateUser", ctx, updateUser2.Username, updateUser2.Email, updateUser2.Password, updateUser2.TenantID).Return(nil).Once()
+	mock.On("UpdateUser", ctx, updateUser2.Username, updateUser2.Email, updateUser2.Password, updateUser2.Password, updateUser2.TenantID).Return(nil).Once()
 
-	err = s.UpdateDataUser(ctx, updateUser2.Username, updateUser2.Email, updateUser2.Password, updateUser2.TenantID)
+	err = s.UpdateDataUser(ctx, updateUser2.Username, updateUser2.Email, updateUser2.Password, updateUser2.Password, updateUser2.TenantID)
 
 	assert.NoError(t, err)
 	mock.AssertExpectations(t)
 
 	// changed password
-	mock.On("UpdateUser", ctx, updateUser3.Username, updateUser3.Email, updateUser3.Password, updateUser3.TenantID).Return(nil).Once()
+	mock.On("UpdateUser", ctx, updateUser3.Username, updateUser3.Email, oldPassword, newPassword, updateUser3.TenantID).Return(nil).Once()
 
-	err = s.UpdateDataUser(ctx, updateUser3.Username, updateUser3.Email, updateUser3.Password, updateUser3.TenantID)
+	err = s.UpdateDataUser(ctx, updateUser3.Username, updateUser3.Email, oldPassword, newPassword, updateUser3.TenantID)
 
 	assert.NoError(t, err)
 	mock.AssertExpectations(t)
